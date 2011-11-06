268c77c4e8ddf024b85d8eb62d196b574218a078
Carlos da Costa <golden.cako+github@gmail.com>
2011-11-05 22:43:45 -0200

    Working page changer and image with the correct scale

--- src/window.py
+++ src/window.py
@@ -14,7 +14,9 @@ class DocumentWidget(QtGui.QWidget):
         self.theparent = parent
         QtGui.QWidget.__init__(self, parent)
         self.click = False
-        self.PdfImage = None
+        self.Document = None
+        self.CurrentPage = None
+        self.Image = None
 
     def paintEvent(self, event):
         """ Sets up a painter and paint with the help of Helper. """
@@ -26,8 +28,13 @@ class DocumentWidget(QtGui.QWidget):
         #red_palette = QtGui.QPalette(QtCore.Qt.red)
         #self.setAutoFillBackground(True)
         #self.setPalette(red_palette)
-        if self.PdfImage:
-            painter.drawImage(self.theparent.rect(), self.PdfImage)
+        if self.Image:
+            page_qsizef = self.CurrentPage.pageSizeF()
+            ratio = page_qsizef.height()/page_qsizef.width()
+            draw_image_rect = self.theparent.rect()
+            # This next line guarantees the Image is not distorted
+            draw_image_rect.setHeight(draw_image_rect.width()*ratio)
+            painter.drawImage(draw_image_rect, self.Image)
         painter.end()
 
     def mousePressEvent(self, event):
@@ -45,17 +52,19 @@ class DocumentWidget(QtGui.QWidget):
         self.click = False
         self.update()
 
-    def loadDocument(self, document):
-        self.document = popplerqt4.Poppler.Document.load(document)
-        self.PdfPage =  self.document.page(0)
-        self.PdfImage = self.PdfPage.renderToImage(300, 300, -1, -1, -1, -1,
-                                                   self.PdfPage.Rotate0)
+    def load_document(self, document):
+        self.Document = popplerqt4.Poppler.Document.load(document)
+        self.CurrentPage =  self.Document.page(0)
+        self.Image = self.CurrentPage.renderToImage(300, 300, -1, -1, -1, -1,
+                                                   self.CurrentPage.Rotate0)
         self.update()
 
-class ExitDialog(QtGui.QDialog):
-    def __init__(self, parent=None):
-        QtGui.QDialog.__init__(self, parent)
-        print uic.loadUi('dialog.ui', self)
+    def change_page_size(self, event):
+        print event
+        self.CurrentPage = self.Document.page(event-1)
+        self.Image = self.CurrentPage.renderToImage(300, 300, -1, -1, -1, -1,
+                                                   self.CurrentPage.Rotate0)
+        self.update()
 
 class MainWindow(QtGui.QMainWindow):
     """ Main Window Class """
@@ -69,21 +78,27 @@ class MainWindow(QtGui.QMainWindow):
                                 [self.actionQuit, self.slot_quit] ]:
             self.connect(action, QtCore.SIGNAL("triggered()"), slot)
 
+        # PDF viewer widget
         self.documentWidget = DocumentWidget(self.scrollAreaWidgetContents)
         self.documentWidget.setObjectName("documentWidget")
         self.documentWidget.setGeometry(QtCore.QRect(0, 0, 916, 335))
         self.gridLayout_3.addWidget(self.documentWidget, 0, 0, 1, 1)
 
+        # Page spinbox widget
+        self.pageSpinBox.setMinimum(1)
+
+        # Connections
+        self.connect(self.pageSpinBox, QtCore.SIGNAL("valueChanged(int)"),
+                     self.documentWidget.change_page_size)
+
     def slot_open(self):
-        """ Slot for openAction. """
+        """ Slot for actionQuit. """
         filename = unicode(QtGui.QFileDialog.getOpenFileName(self, 'Open file'))
         if filename:
-            self.documentWidget.loadDocument(filename)
+            self.documentWidget.load_document(filename)
+            self.pageSpinBox.setValue(1)
             self.statusBar().showMessage('Opened file to %s.' % filename)
-    def slot_quit(self):
-        quitDialog = ExitDialog()
-        print 'this'
-        quitDialog.show()
-        quitDialog.open()
-        print 'that'
 
+    def slot_quit(self):
+        """ Slot for actionQuit. """
+        self.close()
