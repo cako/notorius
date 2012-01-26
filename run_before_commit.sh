b85b9bb6477ac7385d5a7d3a8fce4d123332c684
Carlos da Costa <golden.cako+github@gmail.com>
2012-01-26 15:00:30 -0200

    Correct offset_window.py filename.

--- run_before_commit.sh
+++ run_before_commit.sh
@@ -1,6 +1,6 @@
 sed -e s/`grep 'VERSION = ' ~/Programming/python/notorius/src/window.py | cut -d '%' -f 3`/\'`date +"%y%m%d-%H%M"`\'/ ~/Programming/python/notorius/src/window.py > ~/Programming/python/notorius/src/window_v.py
 mv ~/Programming/python/notorius/src/window_v.py ~/Programming/python/notorius/src/window.py
 pyuic4 -o src/window_ui.py src/window.ui
-pyuic4 -o src/offset_window_ui.ui src/offset_window.ui
+pyuic4 -o src/offset_window_ui.py src/offset_window.ui
 pyuic4 -o src/preamble_window_ui.py src/preamble_window.ui
 rm src/*.pyc
--- src/window.py
+++ src/window.py
@@ -42,7 +42,7 @@ from icons import *
 from PyQt4 import QtCore, QtGui, QtXml
 from xml.etree import ElementTree as xml
 
-VERSION = '0.1.%s' %'120126-1459'
+VERSION = '0.1.%s' %'120126-1500'
 
 class MainWindow(QtGui.QMainWindow):
     """ Main Window Class """
