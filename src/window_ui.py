005ae80eba2587805e45f76c9e9ad6d3bd90a5bb
Carlos da Costa <golden.cako+github@gmail.com>
2012-03-18 14:51:21 -0300

    Minor edit.

--- src/window_ui.py
+++ src/window_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'src/window.ui'
 #
-# Created: Sun Mar 18 13:16:56 2012
+# Created: Sun Mar 18 14:51:02 2012
 #      by: PyQt4 UI code generator 4.7.4
 #
 # WARNING! All changes made in this file will be lost!
